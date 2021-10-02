d59224bec1277976bdea6a17ba5bdb265bd0330a
Daniil <daniil.gerbovets@quantori.com>
2021-10-02 18:16:16 +0300

    Js training

---
+++ JS training.js
@@ -0,0 +1,24 @@
+//Callback's
+/* 
+function ArrayGen(callback){
+    let arr = [1,2,3,4,5];
+    callback(arr);
+    console.log(arr);
+}
+
+function ArrCut(arr_input){
+    arr_input.splice(3,2);
+}
+
+ArrayGen(ArrCut); */
+
+
+//Closure
+
+function variable(){
+    let m = 255;
+    
+
+}
+
+//Promises
